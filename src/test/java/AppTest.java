46d46d88c33a6bd49b8871e36c675aa28e3696de
LouisPham <tupta1999@gmail.com>
2021-12-31 09:17:05 +0700

    add more test case

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -20,6 +20,10 @@ import org.junit.jupiter.api.Test;
  */
 class AppTest {
 	
+	// Total 20		Failures 1		Errors 5
+	
+	// WRONG FORMAT VALIDATION : not complete
+	
 	@Test
 	void emptyLog() {
 		TelephoneBillCalculator calculatorImp = new CalculatorImp();
@@ -30,10 +34,40 @@ class AppTest {
 	}
 	
 	@Test
+	void wrongPhoneFormat() {
+		TelephoneBillCalculator calculatorImp = new CalculatorImp();
+		String logs = "" 
+				+ "42077656235,18-01-2020 08:10:00\n";
+		BigDecimal result = calculatorImp.calculate(logs);
+		BigDecimal expectValue = BigDecimal.valueOf(0);
+		assertEquals(expectValue, result);
+	}
+	
+	@Test
+	void outPhoneFormat() {
+		TelephoneBillCalculator calculatorImp = new CalculatorImp();
+		String logs = "" 
+				+ "420776562354,420776562354,18-01-2020 08:10:00\n";
+		BigDecimal result = calculatorImp.calculate(logs);
+		BigDecimal expectValue = BigDecimal.valueOf(0);
+		assertEquals(expectValue, result);
+	}
+	
+	@Test
+	void outDateFormat() {
+		TelephoneBillCalculator calculatorImp = new CalculatorImp();
+		String logs = "" 
+				+ "420776562354,420776562354,18-01-2020 08:10:00,18-01-2020 08:10:00\n";
+		BigDecimal result = calculatorImp.calculate(logs);
+		BigDecimal expectValue = BigDecimal.valueOf(0);
+		assertEquals(expectValue, result);
+	}	
+	
+	@Test
 	void breakRule() {
 		TelephoneBillCalculator calculatorImp = new CalculatorImp();
 		String logs = "" 
-				+ "420776562354,00,18-01-2020 08:10:00\n";
+				+ "4207765 62354,00,18-01-2020 08:10:00,18-01-2020 08:10:00\n";
 		BigDecimal result = calculatorImp.calculate(logs);
 		BigDecimal expectValue = BigDecimal.valueOf(0);
 		assertEquals(expectValue, result);
@@ -49,6 +83,9 @@ class AppTest {
 		assertEquals(expectValue, result);
 	}
 	
+	// END OF WRONG FORMAT VALIDATION
+	
+	
 	// START DOUPLICATE
 	
 	@Test
@@ -57,10 +94,32 @@ class AppTest {
 		String logs = "" 
 				+ "420776562354,18-01-2020 07:10:00,18-01-2020 08:10:00\n"
 				+ "420776562355,18-01-2020 08:20:00,18-01-2020 08:26:10\n"
+				
 				+ "420776562355,18-01-2020 08:20:00,18-01-2020 08:26:10\n"
 				+ "420776562354,18-01-2020 07:10:00,18-01-2020 08:10:00\n";
 		BigDecimal result = calculatorImp.calculate(logs);
 		BigDecimal expectValue = BigDecimal.valueOf(13.2);
+		assertEquals(expectValue, result);
+	}
+	
+	@Test
+	void sameTriplicate() {
+		TelephoneBillCalculator calculatorImp = new CalculatorImp();
+		String logs = "" 
+				+ "420776562354,18-01-2020 07:10:00,18-01-2020 08:10:00\n"
+				+ "420776562354,18-01-2020 08:20:00,18-01-2020 08:26:10\n"
+				
+				+ "420776562355,18-01-2020 08:20:00,18-01-2020 08:26:10\n"
+				+ "420776562355,18-01-2020 07:10:00,18-01-2020 08:10:00\n"
+				
+				+ "420776562356,18-01-2020 08:20:00,18-01-2020 08:26:10\n"
+				+ "420776562356,18-01-2020 08:20:00,18-01-2020 08:26:10\n"
+				
+				+ "420776562357,18-01-2020 08:20:00,18-01-2020 08:26:10\n"
+				
+				+ "420776562358,18-01-2020 08:20:00,18-01-2020 08:26:10\n";
+		BigDecimal result = calculatorImp.calculate(logs);
+		BigDecimal expectValue = BigDecimal.valueOf(13.2); // not complete
 		assertEquals(expectValue, result);
 	}
 	
